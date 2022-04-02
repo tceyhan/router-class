a5799533b48122ffae6c885d79128aba487dfea3
tceyhan <55tc155@gmail.com>
2022-04-02 15:52:40 +0300

    pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
+import{BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Products from './components/pages/Products';
+import Footer from './components/Footer';
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+     </BrowserRouter>
+
     </div>
   );
 }
